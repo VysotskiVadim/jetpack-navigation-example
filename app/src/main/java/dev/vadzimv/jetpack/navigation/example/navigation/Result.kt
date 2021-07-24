2e7d0c95f6eb3e0207deb86d41422ed4f09b6bfa
vadzimv <vysotski.vadim@gmail.com>
2021-07-24 18:20:37 +0300

    refactoring

--- app/src/main/java/dev/vadzimv/jetpack/navigation/example/navigation/Result.kt
+++ app/src/main/java/dev/vadzimv/jetpack/navigation/example/navigation/Result.kt
@@ -5,26 +5,20 @@ import androidx.annotation.IdRes
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 
 fun <T : Parcelable> NavController.handleResult(
     lifecycleOwner: LifecycleOwner,
     @IdRes currentDestinationId: Int,
-    @IdRes vararg handleResultFrom: Int,
+    @IdRes vararg childrenDestinationsIds: Int,
     handler: (T) -> Unit
 ) {
     // `getCurrentBackStackEntry` doesn't work in case of recovery from the process death when dialog is opened.
     val currentEntry = getBackStackEntry(currentDestinationId)
-    val expectedResultKeys = handleResultFrom.map { resultName(it) }
     val observer = LifecycleEventObserver { _, event ->
         if (event == Lifecycle.Event.ON_RESUME) {
-            for (expectedResultKey in expectedResultKeys) {
-                if (currentEntry.savedStateHandle.contains(expectedResultKey)) {
-                    val result = currentEntry.savedStateHandle.get<T>(expectedResultKey)
-                    handler(result!!)
-                    currentEntry.savedStateHandle.remove<T>(expectedResultKey)
-                }
-            }
+            tryHandleResult(childrenDestinationsIds, currentEntry, handler)
         }
     }
     currentEntry.lifecycle.addObserver(observer)
@@ -33,6 +27,21 @@ fun <T : Parcelable> NavController.handleResult(
             currentEntry.lifecycle.removeObserver(observer)
         }
     })
+}
+
+private fun <T : Parcelable> tryHandleResult(
+    childrenDestinationsIds: IntArray,
+    currentEntry: NavBackStackEntry,
+    handler: (T) -> Unit
+) {
+    for (childrenDestination in childrenDestinationsIds) {
+        val expectedResultKey = resultName(childrenDestination)
+        if (currentEntry.savedStateHandle.contains(expectedResultKey)) {
+            val result = currentEntry.savedStateHandle.get<T>(expectedResultKey)
+            handler(result!!)
+            currentEntry.savedStateHandle.remove<T>(expectedResultKey)
+        }
+    }
 }
 
 fun <T : Parcelable> NavController.finishWithResult(result: T) {
